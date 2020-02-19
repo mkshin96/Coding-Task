ced83c3778f87ea06cd3a0dfa44b547215b8af38
mkshin96 <tlsanrhs96@gmail.com>
2020-02-19 16:16:53 +0900

    프로젝트 생성

---
+++ src/main/java/me/mugon/lendit/LenditApplication.java
@@ -0,0 +1,13 @@
+package me.mugon.lendit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LenditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LenditApplication.class, args);
+    }
+
+}

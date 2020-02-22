867b29248cc7d64a79a2988bfeb686be42f1c389
mkshin96 <tlsanrhs96@gmail.com>
2020-02-22 13:19:48 +0900

    Add IndexController
    GET(/api) 요청 시 서비스의 진입점을 알리는 POST(/api/login)의 link를
    반환

---
+++ src/test/java/me/mugon/lendit/web/IndexControlerTest.java
@@ -0,0 +1,28 @@
+package me.mugon.lendit.web;
+
+import me.mugon.lendit.common.BaseControllerTest;
+import org.junit.jupiter.api.Test;
+
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class IndexControlerTest extends BaseControllerTest {
+
+    @Test
+    void index() throws Exception {
+        mockMvc.perform(get("/api"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("_links.login").exists())
+                .andDo(document("index",
+                        links(
+                                linkWithRel("login").description("link to login")
+                        )
+                ));
+    }
+}
